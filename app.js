acd03e5d57194358f5b4c4aa8549049cd8a0e53a
Öykü Kaya <oykuky001@gmail.com>
2023-03-15 23:16:52 +0300

    added

---
+++ app.js
@@ -0,0 +1,10 @@
+
+
+
+const btnD =document.getElementById("btnClick");
+btnD.addEventListener("click", clicked);
+function clicked(){
+       alert("Butona Tıklandı");
+       document.getElementById("demo").innerHTML = "Hello World";
+       } 
+
