101d77527320d70554be4b54bf856f00512225ea
Öykü Kaya <oykuky001@gmail.com>
2023-03-16 00:12:46 +0300

    uppdate

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 
 
-
 const btnD =document.getElementById("btnClick");
 btnD.addEventListener("click", clicked);
+
 function clicked(){
-       alert("Butona Tıklandı");
-       document.getElementById("demo").innerHTML = "Hello World";
+       if(document.querySelector('#newTask input').value.length == 0){
+              alert("Lütfen görev ekleyiniz.");
+       }
+              else{
+                     document.querySelector('#tasks').innerHTML += `
+                     <div class="tasks">
+                            <span id="taskname">
+                                   ${document.querySelector("#newTask input").value}
+                            </span>
+                            <button class="del">
+                            <i class="far fa-trash-alt"></i>
+                            </button>
+                     </div>
+                     `;
+
+                     var currentTsk=document.querySelectorAll(".del");
+                     for(var i=0;i<currentTsk.length;i++){
+                            currentTsk[i].onclick=function(){
+                                   this.parentNode.remove();
+                            }
+
+                     }
+              }
+
+       
        } 
+
 
