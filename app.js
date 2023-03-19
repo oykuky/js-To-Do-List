6035b214e989c94fffa7daa66f2f2847e1f4723b
Öykü Kaya <oykuky001@gmail.com>
2023-03-20 00:02:26 +0300

    up

--- app.js
+++ app.js
@@ -13,18 +13,33 @@ function clicked(){
                             <span id="taskname">
                                    ${document.querySelector("#newTask input").value}
                             </span>
-                            <button class="delClick" onclick="return ask()">
+                            
+                            <button class="compClick" onclick="return askq()" >
                             <i class="fa-solid fa-circle-check" style="font-size:20px;"></i>
                             </button>
-                            <button class="compClick" >
+                            <button class="delClick" onclick="return ask()">
                             <i class="fa-solid fa-minus"></i>
                             </button>
                             
                      </div>
                      `;
+                     let completedTsk=document.querySelectorAll(".compClick"); //tum complclick butonlarının listesini olusturu
+                     for(let j=0;j<completedTsk.length;j++){
+                            completedTsk[j].onclick=function askq (){
+                                   if(confirm("Task add to completed list ?, Are you confirm?")){}
+                                   else{ return false; }
+                                   let  sty=this.parentNode.querySelector("#taskname") // burada parentNode kullanarak, tıklanan butonun ebeveyn öğesini seçiyoruz
+                                   sty.style.cssText=" text-decoration:line-through; color:green";
+                                   // tıklanan butonun ebeveyn öğesini seçer ve bu öğenin altında #taskname öğesini arar.
+                                 
+                            }
+                         
+                     }
+
+
         
                      var currentTsk=document.querySelectorAll(".delClick");
-                     for(var i=0;i<currentTsk.length;i++){
+                     for(let i=0;i<currentTsk.length;i++){
                             currentTsk[i].onclick=function ask (){
                                    if(confirm("Remove task ?, Are you confirm?")){}
                                    else{ return false; }
@@ -34,7 +49,6 @@ function clicked(){
                          
                      }
 
-                     
                     
               }
 
